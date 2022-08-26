9fd5ad6b845d765ec8079fdc2ea569bc4d3bb561
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-08-26 16:37:50 +0530

    REFACTOR: Stored address book data in local storage

--- src/main/java/com/bridgelabz/addressbook/service/AddressBookServiceImpl.java
+++ src/main/java/com/bridgelabz/addressbook/service/AddressBookServiceImpl.java
@@ -9,31 +9,38 @@ import java.util.List;
 
 @Service
 public class AddressBookServiceImpl implements AddressBookService{
+
+    private List<AddressBookData> addressBookDataList = new ArrayList<>();
     public List<AddressBookData> getAddressBookData(){
-        List<AddressBookData> addressBookDataList = new ArrayList<>();
-        addressBookDataList.add(new AddressBookData(1,new AddressBookDTO("Akanksha", "Jadhav", "female", "Mumbai", "Mumbai", "Maharashtra", 98235, 1029384752, "akanksha@gmail.com")));
         return addressBookDataList;
     }
 
     public AddressBookData getAddressBookDataById(int personId){
-        AddressBookData addressBookData = null;
-        addressBookData = new AddressBookData(1,new AddressBookDTO("Akanksha", "Jadhav", "female", "Mumbai", "Mumbai", "Maharashtra", 98235, 1029384752, "akanksha@gmail.com"));
-        return addressBookData;
+        return addressBookDataList.get(personId-1);
     }
 
     public AddressBookData createAddressBookData(AddressBookDTO addressBookDTO){
         AddressBookData addressBookData = null;
-        addressBookData = new AddressBookData(1, addressBookDTO);
+        addressBookData = new AddressBookData(addressBookDataList.size()+1, addressBookDTO);
+        addressBookDataList.add(addressBookData);
         return addressBookData;
     }
 
-    public AddressBookData updateAddressBookData(AddressBookDTO addressBookDTO){
-        AddressBookData addressBookData = null;
-        addressBookData = new AddressBookData(1, addressBookDTO);
+    public AddressBookData updateAddressBookData(int personId ,AddressBookDTO addressBookDTO){
+        AddressBookData addressBookData = this.getAddressBookDataById(personId);
+        addressBookData.setFirstName(addressBookDTO.firstName);
+        addressBookData.setLastName(addressBookDTO.lastName);
+        addressBookData.setGender(addressBookDTO.gender);
+        addressBookData.setAddress(addressBookDTO.address);
+        addressBookData.setCity(addressBookDTO.city);
+        addressBookData.setState(addressBookDTO.state);
+        addressBookData.setZipCode(addressBookDTO.zipCode);
+        addressBookData.setPhoneNumber(addressBookDTO.phoneNumber);
+        addressBookData.setEmailId(addressBookDTO.emailId);
         return addressBookData;
     }
 
     public void deleteAddressBookData(int personId){
-
+        addressBookDataList.remove(personId-1);
     }
 }
